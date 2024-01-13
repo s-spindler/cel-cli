21b371ab44f9524aa97d2e6b322facb13d3530fd
s-spindler <25225092+s-spindler@users.noreply.github.com>
2024-01-13 22:51:46 +0100

    Move evaluation to separate function

--- main.go
+++ main.go
@@ -9,10 +9,7 @@ import (
 	"github.com/google/cel-go/checker/decls"
 )
 
-func main() {
-	var jsonMap map[string]interface{}
-	json.Unmarshal([]byte(`{"name": "horst"}`), &jsonMap)
-
+func eval(jsonIn map[string]interface{}) (bool, error) {
 	declarations := cel.Declarations(
 		decls.NewVar("i", decls.NewMapType(decls.String, decls.Dyn)),
 	)
@@ -38,9 +35,24 @@ func main() {
 		log.Fatalf("failed to create program: %s", err)
 	}
 
-	out, _, err := program.Eval(map[string]interface{}{"i": jsonMap})
+	out, _, err := program.Eval(map[string]interface{}{"i": jsonIn})
 	if err != nil {
 		log.Fatalf("failed to evaluate program: %s", err)
+	}
+	if out.Type() != cel.BoolType {
+		log.Fatalf("expression did not evaluate to boolean but was of type: %s", out.Type())
+	}
+
+	return out.Value().(bool), nil
+}
+
+func main() {
+	var jsonIn map[string]interface{}
+	json.Unmarshal([]byte(`{"name": "horst"}`), &jsonIn)
+
+	out, err := eval(jsonIn)
+	if err != nil {
+		log.Fatalf("failed to evaluate: %s", err)
 	}
 
 	fmt.Println(out)
