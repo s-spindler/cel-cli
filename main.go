8f755bff01cb00578b1abe5e75c848df2eb69c10
s-spindler <25225092+s-spindler@users.noreply.github.com>
2024-01-13 22:35:01 +0100

    Convert input explicitly to protobuf

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker/decls"
 )
 
 func main() {
-	env, err := cel.NewEnv()
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(`{"name": "horst"}`), &jsonMap)
+	// jsonBytes := []byte(`{"name": "horst"}`)
+	// var prostrct protostruct.Struct
+	// err := protojson.Unmarshal(jsonBytes, &prostrct)
+	// if err != nil {
+	// 	log.Fatalf("failed to convert JSON to proto: %s", err)
+	// }
+
+	declarations := cel.Declarations(
+		decls.NewVar("i", decls.NewMapType(decls.String, decls.Dyn)),
+	)
+
+	env, err := cel.NewEnv(declarations)
 	if err != nil {
 		log.Fatalf("failed to create environment: %s", err)
 	}
 
-	ast, iss := env.Parse(`{'name': 'horst'}`)
+	ast, iss := env.Parse("i.name == 'horst'")
 
 	if iss.Err() != nil {
 		log.Fatalf("failed to parse: %s", iss.Err())
@@ -29,7 +44,7 @@ func main() {
 		log.Fatalf("failed to create program: %s", err)
 	}
 
-	out, _, err := program.Eval(cel.NoVars())
+	out, _, err := program.Eval(map[string]interface{}{"i": jsonMap})
 	if err != nil {
 		log.Fatalf("failed to evaluate program: %s", err)
 	}
