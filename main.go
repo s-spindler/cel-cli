7369c4d210fb7a5dfbcb31843b305183aa6b867d
s-spindler <25225092+s-spindler@users.noreply.github.com>
2024-01-28 23:17:14 +0100

    Add option to force boolean output

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
+	"github.com/google/cel-go/common/types/ref"
 	"github.com/spf13/pflag"
 )
 
-func eval(jsonIn string, expression string) (result bool, err error) {
+func eval(jsonIn string, expression string) (result ref.Val, err error) {
 
 	var jsonMap map[string]interface{}
 	err = json.Unmarshal([]byte(jsonIn), &jsonMap)
@@ -54,24 +55,30 @@ func eval(jsonIn string, expression string) (result bool, err error) {
 		err = fmt.Errorf("failed to evaluate program: %w", err)
 		return
 	}
-	if out.Type() != cel.BoolType {
-		err = fmt.Errorf("expression did not evaluate to boolean but was of type: %s", out.Type())
-		return
-	}
+	// if out.Type() != cel.BoolType {
+	// 	err = fmt.Errorf("expression did not evaluate to boolean but was of type: %s", out.Type())
+	// 	return
+	// }
 
-	result = out.Value().(bool)
-	return result, nil
+	// result = out.Value().(bool)
+
+	return out, nil
 }
 
 func main() {
 	var (
-		jsonIn     string
-		expression string
+		jsonIn         string
+		expression     string
+		boolean_result bool
 	)
 
 	flags := pflag.NewFlagSet("cel-cli", pflag.ExitOnError)
-	flags.StringVarP(&jsonIn, "input-json", "i", "", "JSON input")
-	flags.StringVarP(&expression, "expression", "e", "", "expression to evaluate")
+	flags.StringVarP(&jsonIn, "input-json", "i", "", "JSON input to the expression.")
+	flags.StringVarP(&expression, "expression", "e", "", "The expression to evaluate.")
+	flags.BoolVarP(&boolean_result, "force-bool", "b", false,
+		"Forces the expression to evaluate to a boolean value, terminating with a non-zero "+
+			"status code otherwise. _Note_: Only a true expression will exit the program with "+
+			"0 while false will be non-zero.")
 
 	flags.Parse(os.Args[1:])
 
@@ -80,8 +87,9 @@ func main() {
 		log.Fatalf("failed to evaluate: %s", err)
 	}
 
-	if !result {
-		os.Exit(1)
-	}
+	fmt.Println(result)
+	// if !result {
+	// 	os.Exit(1)
+	// }
 	// else: fall through and return default exit code 0
 }
